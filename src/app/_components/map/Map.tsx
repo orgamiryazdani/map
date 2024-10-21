288529f4cbfffd24f6d74080740a68e204944691
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-21 19:18:05 +0330

    Changing the folder strategy

--- src/app/_components/map/Map.tsx
+++ src/app/_components/map/Map.tsx
@@ -1,32 +1,33 @@
 "use client";
 
 import "leaflet/dist/leaflet.css";
-import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const Map: React.FC = () => {
-  const [position, setPosition] = useState<[number, number]>([51.505, -0.09]);
+export const Map: React.FC = () => {
   return (
     <section className='md:w-[94vw] w-[90vw] h-[90vh] z-50 relative'>
-        <input type="text" className="absolute w-[70%] h-10 rounded-full bg-secondary top-3 right-3 shadow-md z-50 px-3 text-sm outline-none text-primary" placeholder="جستجو..."  name="" id="" />
-        <div className="absolute sm:w-[370px] w-72 h-32 rounded-3xl background bottom-3 left-[50%] -translate-x-[50%] shadow-md z-50"></div>
+      <input
+        type='text'
+        className='absolute w-[70%] h-10 rounded-full bg-secondary top-3 right-3 shadow-md z-50 px-3 text-sm outline-none text-primary'
+        placeholder='جستجو...'
+        name=''
+        id=''
+      />
       <MapContainer
-      className="w-full h-full rounded-3xl z-40"
-        center={position}
+        className='w-full h-full rounded-3xl z-40'
+        center={[51.505, -0.09]}
         zoom={13}
         scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-         <Marker position={position}>
+        <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker> *
+        </Marker>
       </MapContainer>
     </section>
   );
 };
-
-export default Map;
