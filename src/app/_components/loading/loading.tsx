419d1900d3281df0d8ad357dec33d2a7f0ddbe94
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-29 16:39:48 +0330

    feat: add loading component with styles and stories
    - Added `loading` component in `src/app/_components/loading/loading.tsx`.
    - Created corresponding story file `loading.stories.tsx` for component preview.
    - Defined `loading.types.ts` for type definitions.
    - Updated Tailwind CSS with `loading.css` styles.
    - Removed `test.tsx` as it was no longer needed.

---
+++ src/app/_components/loading/loading.tsx
@@ -0,0 +1,26 @@
+import classNames from "classnames";
+import { LoadingProps } from "./loading.types";
+import { Size } from "@/types/size.type";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "loading-xs",
+  small: "loading-sm",
+  normal: "loading-md",
+  large: "loading-lg",
+};
+
+export const Loading: React.FC<LoadingProps> = ({
+  type = "spinner",
+  variant,
+  size = "normal",
+  className,
+}: LoadingProps) => {
+  const classes = classNames(
+    "loading",
+    className,
+    { [`loading-${type}`]: type },
+    { [`${sizeClasses[size]}`]: size },
+    { [`loading-${variant}`]: variant },
+  );
+  return <span className={classes}></span>;
+};
