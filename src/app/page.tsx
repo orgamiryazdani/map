a3b6cfe8f244fb098e87b87ce81a674e76c66d0c
orgamiryazdani <orgamiryazdani@gmail.com>
2024-10-31 13:45:06 +0330

    style: change home page style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Map } from "./_components/map";
 
 export default function Home() {
   return (
-    <div >
+    <div className="col-start-2 col-end-13 row-start-2 row-end-8 pb-6 pl-6">
       <Map />
     </div>
   );
