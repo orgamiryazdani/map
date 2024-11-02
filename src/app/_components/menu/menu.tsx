9f717a97777cffeb707b3b8b325d071150f46230
orgamiryazdani <orgamiryazdani@gmail.com>
2024-11-02 23:40:58 +0330

    fix(menu): fix type bug

--- src/app/_components/menu/menu.tsx
+++ src/app/_components/menu/menu.tsx
@@ -34,14 +34,14 @@ const menuItems = [
   },
 ];
 
-const Menu = () => {
+const Menu: React.FC = () => {
   const pathname = usePathname();
 
   return (
     <div className='col-start-1 col-end-2 row-start-1 row-end-13 h-svh flex items-center justify-center'>
       <div className='navigation relative w-[70px] h-[93svh] dark:bg-base-content flex justify-center items-center rounded-2xl'>
         <div className='w-[51px] h-[58px] rounded-2xl dark:bg-base-100 font-bold text-2xl absolute top-4 flex items-center justify-center'>
-          <IconMap stroke="#fff"/>
+          <IconMap stroke='#fff' />
         </div>
         <ul className='flex flex-col w-[70px] h-[100%] pt-[115px]'>
           {menuItems.map((item) => (
@@ -61,8 +61,8 @@ const Menu = () => {
           ))}
           <div className='indicator absolute right-[65%] w-[55px] h-[120px] dark:bg-base-100 border-[6px] border-base-100 rounded-[20px] flex justify-center items-center duration-300 rotate-[270deg]'></div>
         </ul>
-        <div className="absolute bottom-5">
-          <IconMoon stroke="#000" />
+        <div className='absolute bottom-5'>
+          <IconMoon stroke='#000' />
         </div>
       </div>
     </div>
