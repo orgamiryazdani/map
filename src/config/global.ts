446bf7f0db60cc1df12ec54f99d9406e45e30af9
orgamiryazdani <orgamiryazdani@gmail.com>
2024-11-02 23:04:40 +0330

    fix(config global): fix import API_KEY bug

--- src/config/global.ts
+++ src/config/global.ts
@@ -1,4 +1,5 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 export const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
