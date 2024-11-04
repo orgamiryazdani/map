1a7c3a698e23bdb36988abbda48decd540a59386
orgamiryazdani <orgamiryazdani@gmail.com>
2024-11-04 23:20:36 +0330

    feat(icon): add new icon

--- src/app/_components/icons/icons.ts
+++ src/app/_components/icons/icons.ts
@@ -3,8 +3,11 @@
 export { default as IconAdd } from './src/add';
 export { default as IconArrowRight } from './src/arrow-right';
 export { default as IconBookmark } from './src/bookmark';
+export { default as IconCheck } from './src/check';
 export { default as IconClose } from './src/close';
+export { default as IconError } from './src/error';
 export { default as IconHome } from './src/home';
+export { default as IconInfo } from './src/info';
 export { default as IconLayer } from './src/layer';
 export { default as IconLocation } from './src/location';
 export { default as IconMap } from './src/map';
