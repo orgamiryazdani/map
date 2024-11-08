946d0307deeb91ec69a40685cf49bf35291e526b
orgamiryazdani <orgamiryazdani@gmail.com>
2024-11-08 23:38:56 +0330

    feat(hooks): Writing a custom hook to update the url dynamically

---
+++ src/hooks/useUpdateLocation.ts
@@ -0,0 +1,30 @@
+import { useRouter, usePathname } from "next/navigation";
+import { useCallback } from "react";
+
+type LocationData = {
+    lat?: number;
+    lng?: number;
+    livelat?: number;
+    livelng?: number;
+    pinlat?: number | null;
+    pinlng?: number | null;
+};
+
+const useUpdateLocation = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const updateLocation = useCallback(
+        (locationData: LocationData) => {
+            const params = new URLSearchParams(window.location.search);
+            Object.entries(locationData).forEach(([key, value]) => {
+                params.set(key, String(value));
+            });
+            router.replace(`${pathname}?${params.toString()}`);
+        },
+        [router, pathname]
+    );
+    return updateLocation;
+};
+
+export default useUpdateLocation;
