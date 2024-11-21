c6723e08c3ac31006963f7130a5debbb89cdb2c3
orgamiryazdani <orgamiryazdani@gmail.com>
2024-11-21 22:45:33 +0330

    feat: Using trucateText to manage texts and change card styles

--- src/app/(save-location)/_components/location-card.tsx
+++ src/app/(save-location)/_components/location-card.tsx
@@ -2,6 +2,7 @@
 import { IconDelete } from "@/app/_components/icons/icons";
 import { useLocationsStore } from "@/stores/locations.store";
 import { FormLocationValue } from "@/types/location.interface";
+import truncateText from "@/utils/truncateText";
 import Link from "next/link";
 
 const LocationCard: React.FC<{ location: FormLocationValue }> = ({
@@ -13,12 +14,14 @@ const LocationCard: React.FC<{ location: FormLocationValue }> = ({
   const { address, categoryName, lat, lng, placeName } = location;
 
   return (
-    <Link
-      href={`/?lat=${lat}&lng=${lng}`}
-      className='w-[31.7%] h-auto max-h-44 rounded-2xl text-base-50 saveLocationCardBg p-3 cursor-pointer flex flex-col justify-between gap-y-1'>
-      <p className='font-black text-2xl'>{placeName}</p>
-      <p className='font-bold text-lg'>{address}</p>
-      <p className='font-semibold'>دسته بندی : {categoryName}</p>
+    <div className='w-[31.7%] h-auto max-h-44 rounded-2xl text-base-50 saveLocationCardBg p-3 flex flex-col justify-between'>
+      <Link
+        href={`/?lat=${lat}&lng=${lng}`}
+        className='h-[113px] flex flex-col justify-between'>
+        <p className='font-black text-2xl'>{truncateText(placeName, 27)}</p>
+        <p className='font-bold text-lg'>{truncateText(address, 35)}</p>
+        <p className='font-semibold'>دسته بندی : {categoryName}</p>
+      </Link>
       <div className='flex justify-between'>
         <div className='flex items-center gap-x-3 [&>*]:bg-base-25 [&>*]:px-2 [&>*]:text-secondary-content [&>*]:rounded-md [&>*]:h-6 [&>*]:pt-0.5'>
           <p>{lat}</p>
@@ -26,10 +29,13 @@ const LocationCard: React.FC<{ location: FormLocationValue }> = ({
         </div>
         <div className='flex items-center gap-x-2'>
           {/* <IconEdit /> */}
-          <IconDelete onClick={() => deleteLocation(lat, lng)} />
+          <IconDelete
+            onClick={() => deleteLocation(lat, lng)}
+            className='cursor-pointer'
+          />
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
